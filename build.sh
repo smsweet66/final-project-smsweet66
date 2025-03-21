25026e4869460c12809e76718ae6bbbc3d2d3c9c
Sean Sweet <sweet.sean42@gmail.com>
2025-03-20 19:53:44 -0500

    using functions

--- build.sh
+++ build.sh
@@ -9,38 +9,42 @@ git submodule update
 # local.conf won't exist until this step on first execution
 source poky/oe-init-build-env rpi-build
 
-CONFLINE="MACHINE = \"raspberrypi0-2w-64\""
-
-cat conf/local.conf | grep "${CONFLINE}" > /dev/null
-local_conf_info=$?
-
-if [ $local_conf_info -ne 0 ];then
-	echo "Append ${CONFLINE} in the local.conf file"
-	echo ${CONFLINE} >> conf/local.conf
+add_configuration() {
+	CONFLINE=$1
 	
-else
-	echo "${CONFLINE} already exists in the local.conf file"
-fi
+	cat conf/local.conf | grep "${CONFLINE}" > /dev/null
+	local_conf_info=$?
+
+	if [ $local_conf_info -ne 0 ];then
+		echo "Append ${CONFLINE} in the local.conf file"
+		echo ${CONFLINE} >> conf/local.conf
+	else
+		echo "${CONFLINE} already exists in the local.conf file"
+	fi
+}
+
+add_layer() {
+	LAYER=$1
+
+	bitbake-layers show-layers | grep $LAYER > /dev/null
+	layer_info=$?
+
+	if [ $layer_info -ne 0 ]; then
+		echo "Adding $LAYER layer"
+		bitbake-layers add-layer ../$LAYER
+	else
+		echo "$LAYER layer already exists"
+	fi
+}
+
+add_configuration "MACHINE = \"raspberrypi0-2w-64\""
+add_configuration "LICENSE_FLAGS_ACCEPTED = \"synaptics-killswitch\""
+
+add_layer "meta-raspberrypi"
+add_layer "meta-thermometer"
 
 bitbake-layers show-layers | grep "meta-raspberrypi" > /dev/null
 layer_info=$?
-
-if [ $layer_info -ne 0 ]; then
-	echo "Adding meta-raspberrypi layer"
-	bitbake-layers add-layer ../meta-raspberrypi
-else
-	echo "meta-raspberrypi layer already exists"
-fi
-
-bitbake-layers show-layers | grep "meta-thermometer" > /dev/null
-layer_info=$?
-
-if [ $layer_info -ne 0 ]; then
-	echo "Adding meta-thermometer layer"
-	bitbake-layers add-layer ../meta-thermometer
-else
-	echo "meta-thermometer layer already exists"
-fi
 
 set -e
 bitbake core-image-thermometer
