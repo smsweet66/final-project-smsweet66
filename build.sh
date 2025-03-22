3e18360b3eef31837b6af252023a7a577dfd0ced
Sean Sweet <sweet.sean42@gmail.com>
2025-03-22 15:23:26 -0500

    fix typo

--- build.sh
+++ build.sh
@@ -61,7 +61,7 @@ add_configuration "CORE_IMAGE_EXTRA_INSTALL += \"openssh\""
 add_configuration "DISTRO_FEATURES:append = \"bluez5 bluetooth wifi\""
 
 add_layer "meta-raspberrypi" ".."
-add_layer "meta-oe" "../meta-openebedded"
+add_layer "meta-oe" "../meta-openembedded"
 add_layer "meta-python" "../meta-openembedded"
 add_layer "meta-networking" "../meta-openembedded"
 add_layer "meta-thermometer" ".."
