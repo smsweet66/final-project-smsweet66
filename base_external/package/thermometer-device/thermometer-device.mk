80a7bee8f265148e4d9f2250fffbd4a2b5ef102c
Sean Sweet <sweet.sean42@gmail.com>
2025-04-08 21:33:14 -0500

    updated thermometer version

--- base_external/package/thermometer-device/thermometer-device.mk
+++ base_external/package/thermometer-device/thermometer-device.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-THERMOMETER_DEVICE_VERSION = 1191747
+THERMOMETER_DEVICE_VERSION = cba1353
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
