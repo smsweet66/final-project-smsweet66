3f522b0d03bd9584ec31a7d4e74e7c7ec1b735d5
Sean Sweet <sweet.sean42@gmail.com>
2025-04-12 12:27:46 -0500

    cleanup

--- base_external/package/thermometer-client/thermometer-client.mk
+++ base_external/package/thermometer-client/thermometer-client.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-THERMOMETER_CLIENT_VERSION = 66bc253
+THERMOMETER_CLIENT_VERSION = 5a9d65b
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
