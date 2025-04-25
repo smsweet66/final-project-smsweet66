6f62b132970f525bcc357ed02c285cbb37fa3a58
Sean Sweet <sweet.sean42@gmail.com>
2025-04-24 19:07:58 -0500

    got thermometer device working

--- base_external/package/thermometer-device/thermometer-device.mk
+++ base_external/package/thermometer-device/thermometer-device.mk
@@ -5,7 +5,7 @@
 #
 ##############################################################
 
-THERMOMETER_DEVICE_VERSION = 8cd2a84
+THERMOMETER_DEVICE_VERSION = 52dadde
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -13,7 +13,7 @@ THERMOMETER_DEVICE_SITE = git@github.com:smsweet66/thermometer-device-driver.git
 THERMOMETER_DEVICE_SITE_METHOD = git
 THERMOMETER_DEVICE_GIT_SUBMODULES = YES
 
-THERMOMETER_DEVICE_MODULE_SUBDIRS +=  src
+THERMOMETER_DEVICE_MODULE_SUBDIRS += src
 
 THERMOMETER_DEVICE_MODULE_MAKE_OPTS = KVERSION=$(LINUX_VERSION_PROBED)
 
